1d08ad40d5d58adbfe63cd0996b415057749a7ea
Yuna-commits <ksm13563661@gmail.com>
2025-10-24 12:30:30 +0900

    comment: BootStrapping Method 주석 수정

--- jblog07/src/main/java/com/bit2025/jblog/JBlogApplication.java
+++ jblog07/src/main/java/com/bit2025/jblog/JBlogApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @SpringBootApplication
- * = ( @Configuration + @EnableAutoConfiguration + @ComponentScan )
+ * - @SpringBootConfiguration + @EnableAutoConfiguration + @ComponentScan
  */
 @SpringBootApplication
 public class JBlogApplication {
@@ -15,10 +15,12 @@ public class JBlogApplication {
 		 * BootStrapping Method
 		 * 
 		 * SpringApplication.run(...) 호출
-		 * > 1. Spring ApplicationContext 생성
-		 * > 2. Auto-Configuration
-		 * > 3. 내장 톰캣 서버 시작
-		 * > 4. Bean 초기화, 의존성 주입 수행
+		 * > 1. SpringApplication 객체 생성, 환경 초기화
+		 * > 2. ApplicationContext 생성
+		 * > 3. @EnableAutoConfiguration에 따라 자동 설정 클래스 로드
+		 * > 4. @ComponentScan으로 사용자 정의 Bean 스캔, 등록
+		 * > 5. 내장 톰캣 서버 생성, 시작
+		 * > 6. Bean 초기화, 의존성 주입 수행
 		 */
 		SpringApplication.run(JBlogApplication.class, args);
 	}
